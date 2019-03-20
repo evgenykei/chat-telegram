34923a37199db5d0f0b64f7ee99e69eede70fab1
Nikita <slayerdarkfly@gmail.com>
2019-03-20 16:36:48 +0700

    calendarBuilder finished

--- src/functions/calendar/calendarBuilder.ts
+++ src/functions/calendar/calendarBuilder.ts
@@ -3,66 +3,121 @@ import * as moment from 'moment'
 
 import Node from '../../menu/Node'
 
-export function BuildYears(callbackData: string, year?: number, minDate?: Date, maxDate?: Date): Node[][] {
-  if (!year) year = moment().year()
-  const min = minDate ? moment(minDate).year() : 1990,
-        max = maxDate ? moment(maxDate).year() : 2050,
-        from = Math.floor((year + 1) / 10) * 10,
-        to = from + 9
+const MINDATE = moment({ year: 1990 }),
+      MAXDATE = moment({ year: 2049 }).endOf('year')
+
+export function BuildYears(node: Node,
+                           curDate?: moment.Moment, minDate?: moment.Moment, maxDate?: moment.Moment): Node[][] {
+
+  if (!curDate) curDate = moment()
+  if (!minDate) minDate = MINDATE
+  if (!maxDate) maxDate = MAXDATE
+
+  const from = moment({ year: Math.floor(curDate.year() / 10) * 10 }),
+        to = from.clone().add(9, 'years')
 
   const head = [
-    inRange(from - 2, min, max)
-      ? new Node(`${callbackData}.${from - 2}`, '<')
-      : new Node(`${callbackData}.noaction`, ' '),
+    inBounds(from.clone().subtract(1, 'years'), minDate, maxDate, 'year')
+      ? new Node(`${node.id}.${from.year() - 2}`, '<')
+      : new Node(`${node.id}.noaction`, ' '),
 
-    new Node(`${callbackData}.noaction`, `${from} - ${to}`),
+    new Node(`${node.id}.noaction`, `${from.year()} - ${to.year()}`),
 
-    inRange(to + 2, min, max)
-      ? new Node(`${callbackData}.${to + 2}`, '>')
-      : new Node(`${callbackData}.noaction`, ' '),
+    inBounds(to.clone().add(1, 'years'), minDate, maxDate, 'year')
+      ? new Node(`${node.id}.${to.year() + 2}`, '>')
+      : new Node(`${node.id}.noaction`, ' '),
   ]
 
-  const body = _.range(from - 1, to + 2).map(y => {
-    const validYear = inRange(y, min, max)
-    const id = validYear ? `${callbackData}.${y};0` : `${callbackData}.noaction`
+  const body = _.range(from.year() - 1, to.year() + 2).map(y => {
+    const validYear = inBounds(moment({ year: y }), minDate!, maxDate!, 'year')
+    const id = validYear ? `${node.id}.${y};0` : `${node.id}.noaction`
     const title = validYear ? y.toString() : ' '
     return new Node(id, title)
   })
 
-  return _.chunk(_.union(head, body), 3)
+  const result = _.chunk(_.concat(head, body), 3)
+  if (node.parent) result.unshift([new Node(node.parent!.id, 'button.back')])
+  return result
 }
 
-export function BuildMonths(callbackData: string, iso?: string,
-                            year?: number, minDate?: Date,  maxDate?: Date): Node[][] {
+export function BuildMonths(node: Node, iso?: string,
+                            curDate?: moment.Moment, minDate?: moment.Moment, maxDate?: moment.Moment): Node[][] {
 
-  if (!year) year = moment().year()
-  const minYear = minDate ? moment(minDate).year() : 1990,
-        maxYear = maxDate ? moment(maxDate).year() : 2050,
-        minMonth = minDate && year === minYear ? moment(minDate).month() : 0,
-        maxMonth = maxDate && year === maxYear ? moment(maxDate).month() : 11
+  if (!iso) iso = 'en'
+  if (!curDate) curDate = moment()
+  if (!minDate) minDate = MINDATE
+  if (!maxDate) maxDate = MAXDATE
 
   const head = [
-    inRange(year - 1, minYear, maxYear)
-      ? new Node(`${callbackData}.${year - 1};0`, '<')
-      : new Node(`${callbackData}.noaction`, ' '),
+    inBounds(curDate.clone().subtract(1, 'years'), minDate, maxDate, 'year')
+      ? new Node(`${node.id}.${curDate.year() - 1};0`, '<')
+      : new Node(`${node.id}.noaction`, ' '),
 
-    new Node(`${callbackData}.noaction`, `${year}`),
+    new Node(`${node.id}.${curDate!.year()}`, `${curDate.year()}`),
 
-    inRange(year + 1, minYear, maxYear)
-      ? new Node(`${callbackData}.${year + 1};0`, '>')
-      : new Node(`${callbackData}.noaction`, ' '),
+    inBounds(curDate.clone().add(1, 'years'), minDate, maxDate, 'year')
+      ? new Node(`${node.id}.${curDate.year() + 1};0`, '>')
+      : new Node(`${node.id}.noaction`, ' '),
   ]
 
   const body = _.range(0, 12).map(m => {
-    const validMonth = inRange(m, minMonth, maxMonth)
-    const id = validMonth ? `${callbackData}.${year};${m};0` : `${callbackData}.noaction`
+    const validMonth = inBounds(curDate!.clone().month(m), minDate!, maxDate!, 'month')
+    const id = validMonth ? `${node.id}.${curDate!.year()};${m};0` : `${node.id}.noaction`
     const title = validMonth ? _.capitalize(moment().locale(iso || 'en').month(m).format('MMM')) : ' '
     return new Node(id, title)
   })
 
-  return _.chunk(_.union(head, body), 3)
+  const result = _.chunk(_.concat(head, body), 3)
+  if (node.parent) result.unshift([new Node(node.parent!.id, 'button.back')])
+  return result
 }
 
-function inRange(cur: number, min: number, max: number) {
-  return cur >= min && cur <= max
+export function BuildDays(node: Node, iso?: string,
+                          curDate?: moment.Moment, minDate?: moment.Moment, maxDate?: moment.Moment): Node[][] {
+
+  if (!iso) iso = 'en'
+  if (!curDate) curDate = moment()
+  if (!minDate) minDate = MINDATE
+  if (!maxDate) maxDate = MAXDATE
+
+  const prevMonthDate = curDate.clone().subtract(1, 'months'),
+        nextMonthDate = curDate.clone().add(1, 'months')
+
+  const head = [
+    inBounds(prevMonthDate, minDate, maxDate, 'month')
+      ? new Node(`${node.id}.${prevMonthDate.year()};${prevMonthDate.month()};0`, '<')
+      : new Node(`${node.id}.noaction`, ' '),
+
+    new Node(`${node.id}.${curDate!.year()};${curDate!.month()}`,
+      _.capitalize(curDate.locale(iso).format('MMMM, YYYY'))),
+
+    inBounds(nextMonthDate, minDate, maxDate, 'month')
+      ? new Node(`${node.id}.${nextMonthDate.year()};${nextMonthDate.month()};0`, '>')
+      : new Node(`${node.id}.noaction`, ' '),
+  ]
+
+  const weekDays = _.range(0, 7).map(d =>
+    new Node(`${node.id}.noaction`, _.capitalize(moment().locale(iso!).weekday(d).format('ddd'))))
+
+  const isoFirstWeekDay = moment.localeData(iso).firstDayOfWeek() - 1
+  const body = _.chunk(_.range(1 + isoFirstWeekDay, 43 + isoFirstWeekDay).map(it => {
+    const day = curDate!.clone().day(it)
+    const validDay = day.month() === curDate!.month() && inBounds(day, minDate!, maxDate!, 'day')
+    const id = validDay
+      ? `${node.id}.${curDate!.year()};${curDate!.month()};${day.day()};0`
+      : `${node.id}.noaction`
+    const title = validDay ? day.format('DD') : '  '
+    return new Node(id, title)
+  }), 7).filter(it => it.some(val => val.title.trim().length !== 0))
+
+  const result = _.concat([head], [weekDays], body)
+  if (node.parent) result.unshift([new Node(node.parent!.id, 'button.back')])
+  return result
+}
+
+function inBounds(cur: moment.Moment, min: moment.Moment, max: moment.Moment, unit: moment.unitOfTime.StartOf) {
+  const curClone = cur.clone().startOf(unit),
+        minClone = min.clone().startOf(unit),
+        maxClone = max.clone().startOf(unit)
+  return curClone >= minClone && curClone <= maxClone
 }
--- src/functions/index.ts
+++ src/functions/index.ts
@@ -78,7 +78,7 @@ export const resetPassword: IFunction = async (body: IFunctionBody) => {
     await body.bot.answerCallbackQuery(chatId, callbackQueryId, 'text.notImplemented')
 }
 
-// Запросить количество дней отпуска TODO
+// Запросить количество дней отпуска
 export const requestVacationDays: IFunction = async (body: IFunctionBody) => {
   const { args, node, localeService } = body
   const { chatId, callbackQueryId, messageId } = body.messageBody
@@ -87,33 +87,45 @@ export const requestVacationDays: IFunction = async (body: IFunctionBody) => {
 
   // send calendar
   if (!args) {
-    const buttons = _.union(
-      [[new Node(node.parent!.id, 'button.back')]],
-      Calendar.BuildYears(node.id, undefined, new Date()),
-    )
+    const buttons = Calendar.BuildYears(node, undefined, moment())
     body.bot.updateMenu(body.messageBody.chatId, messageId, buttons)
   }
+
   else if (args[0] === 'noaction')
     await body.bot.answerCallbackQuery(chatId, callbackQueryId)
+
+  // Send years
   else if (args.length === 1) {
-    const buttons = _.union(
-      [[new Node(node.parent!.id, 'button.back')]],
-      Calendar.BuildYears(node.id, parseInt(args[0], 10), new Date()),
-    )
+    const argsParsed = args.map(it => parseInt(it, 10)),
+          curDate = moment({ year: argsParsed[0] })
+
+    const buttons = Calendar.BuildYears(node, curDate, moment())
     body.bot.updateMenu(body.messageBody.chatId, messageId, buttons)
   }
+
+  // Send months
   else if (args.length === 2) {
     const locale = await localeService.getChatLocale(chatId),
-          iso = locale ? locale.iso : undefined
-    const buttons = _.union(
-      [[new Node(node.parent!.id, 'button.back')]],
-      Calendar.BuildMonths(node.id, iso, parseInt(args[0], 10), new Date()),
-    )
-    body.bot.updateMenu(body.messageBody.chatId, messageId, buttons)
+          iso = locale ? locale.iso : undefined,
+          argsParsed = args.map(it => parseInt(it, 10)),
+          curDate = moment({ year: argsParsed[0] })
 
+    const buttons = Calendar.BuildMonths(node, iso, curDate, moment())
+    body.bot.updateMenu(body.messageBody.chatId, messageId, buttons)
   }
-  else if (args.length === 3)
-    await body.bot.answerCallbackQuery(chatId, callbackQueryId, 'text.notImplemented')
+
+  // Send days
+  else if (args.length === 3) {
+    const locale = await localeService.getChatLocale(chatId),
+          iso = locale ? locale.iso : undefined,
+          argsParsed = args.map(it => parseInt(it, 10)),
+          curDate = moment({ year: argsParsed[0], month: argsParsed[1] })
+
+    const buttons = Calendar.BuildDays(node, iso, curDate, moment())
+    body.bot.updateMenu(body.messageBody.chatId, messageId, buttons)
+  }
+  // Send result TODO
+  else if (args.length === 4) await body.bot.answerCallbackQuery(chatId, callbackQueryId, 'text.notImplemented')
   else throw new Error('Wrong args length')
 }
 
