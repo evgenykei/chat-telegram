37bc12dfee9163f6d37910e8e86ad4c65ae61b0b
Nikita <slayerdarkfly@gmail.com>
2019-03-23 16:52:02 +0700

    ClassifierAPI Fix

--- src/api/classifier/ClassifierAPI.ts
+++ src/api/classifier/ClassifierAPI.ts
@@ -7,7 +7,7 @@ import { config } from 'node-config-ts'
 export default class ClassifierAPI implements IClassifierAPI {
 
   public async getClassName(text: string): Promise<string> {
-    const req = await axios.get(url.resolve(config.urls.classifier, 'api/predict' + encodeURIComponent(text)))
+    const req = await axios.get(url.resolve(config.urls.classifier, 'api/predict/' + encodeURIComponent(text)))
     return req.data
   }
 
